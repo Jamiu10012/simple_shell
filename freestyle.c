32d7acc41ae661b91307f9e92e6d5d4ae96bba56
Ibrahim Jamiu <ibrahimjamiu026@gmail.com>
2023-11-03 14:10:52 -0700

    on first task

---
+++ freestyle.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+#include "unistd.h"
+
+/**
+* print_freestyle - the main function freestyle
+* Return: return sucess
+*
+*/
+
+void print_freestyle(void)
+{
+printf("Hello_FreestyleWorld\n");
+}
+
+/**
+* main - main fun
+* Return: return success
+* Mounntain and silent
+*/
+
+int main(void)
+{
+printf("This is main function\n");
+print_freestyle();
+return (0);
+}
