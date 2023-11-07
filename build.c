d5fa27b8d52c5462c16c4f90d2ae6fdf6f2247a0
EmEch01 <rhamzee13@gmail.com>
2023-11-07 13:26:16 -0800

    Task 1/Task 2

---
+++ build.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 1000
+/**
+ * build_command - Function to read and execute user command
+ * @command: Display promt for user
+ * Author: EmEch and Silent Coder
+ */
+
+void build_command(char *command)
+{
+printf("simple_shell> ");
+fgets(command, MAX_COMMAND_LENGTH, stdin);
+command[strcspn(command, "\n")] = '\0';
+}
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0.
+ * Or Errno
+ */
+
+int main(void)
+{
+char command[MAX_COMMAND_LENGTH];
+int status;
+
+while (1)
+{
+build_command(command);
+
+if (strlen(command) == 0)
+{
+continue;
+}
+
+pid_t pid = fork();
+
+if (pid == -1)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+
+if (pid == 0)
+{
+if (execlp(command, command, NULL) == -1)
+{
+perror("execlp");
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+wait(&status);
+}
+}
+
+return (0);
+}
---
+++ build_args.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 1000
+
+/**
+ * build_command_args - command arguments
+ * @command args: Handles all arguments in commands
+ * Author by EmEch and Silent Coder
+ */
+
+void build_command_args(char *command)
+{
+printf("simple_shell> ");
+fgets(command, MAX_COMMAND_LENGTH, stdin);
+command[strcspn(command, "\n")] = '\0';
+}
+
+/**
+ * execute_command - execute command
+ * @args: Handles and executes arguments
+ */
+
+void execute_command(char **args)
+{
+pid_t pid = fork();
+if (pid == 0)
+{
+if (execvp(args[0], args) == -1)
+{
+perror("execlp");
+exit(EXIT_FAILURE);
+}
+}
+else if (pid < 0)
+{
+perror("fork");
+}
+else
+{
+wait(NULL);
+}
+}
+
+/**
+ * main - Reads command  length and size
+ * Author: EmEch and SilentCoder
+ * Return: Always 0.
+ * Or Errno upon Failure
+ */
+
+int main(void)
+{
+char command[MAX_COMMAND_LENGTH];
+while (1)
+{
+build_command_args(command);
+if (strcmp(command, "exit") == 0)
+{
+printf("See you again!\n");
+break;
+}
+if (strlen(command) > 0)
+{
+int arg_count = 0;
+char *token = strtok(command, " ");
+char *args[MAX_COMMAND_LENGTH];
+while (token != NULL)
+{
+args[arg_count] = token;
+arg_count++;
+token = strtok(NULL, " ");
+}
+args[arg_count] = NULL;
+if (arg_count > 0)
+{
+execute_command(args);
+}
+}
+}
+return (0);
+}
