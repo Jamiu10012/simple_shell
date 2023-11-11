3374e70c0de3bd875f5110ad0fa4eb070598c390
EmEch01 <rhamzee13@gmail.com>
2023-11-10 17:00:02 -0800

    build_path.c

--- build_path.c
+++ build_path.c
@@ -8,83 +8,104 @@
 #define MAX_INPUT_LENGTH 1024
 #define COMMAND_LENGTH 100
 
-/* find the command in specified directories */
-int find_command(char *command, char **directories, char *buffer)
+/**
+ * find_command - Reads the command
+ * @char: to read directories and buffer
+ * Code by EmEch and Silentcoder
+ * Return: Always 0.
+ */
+
+int find_command(char **directories, char *command, char *buffer)
 {
-	for (int e = 0, path_dir[e] != NULL; ++)
-	{
-		char path[BUFFER SIZE]
-			
-		strcpy(path, directories[e]);
-		strcat(path, "/");
-		strcat(path, cmd);
-		
-		if (access(path, cmd, X_OK) == 0)
-	{
-		strcpy(buffer, path);
-		return;
-	}
-	}
-/* Taking maximum of 2 directories in PATH */
+for (int e = 0; directories[e] != NULL; i++)
+{
+char path[MAX_PATH_LENGTH];
+strcpy(path, directories[i]);
+strcat(path, "/");
+strcat(path, command);
+
+if (access(path, Y_OK) == 0)
+{
+strcpy(buffer, path);
+return (0);
+}
+}
+
+return (-1);
+}
+
+/**
+ * execute_command - Executes the command readed in stdin using execvp
+ * @args: command and arguments passed to path or directory
+ *
+ * Return: Always Error.
+ */
+
+int execute_command(char *command_path, char **arguments)
+{
+pid_t pid = fork();
+if (pid == 0)
+{
+execvp(command_path, arguments);
+perror("Error executing command");
+exit(EXIT_FAILURE);
+}
+else if (pid < 0)
+{
+perror("Fork failed");
+}
+else
+{
+wait(NULL);
+}
+return (0);
+}
+
+/**
+ * main - Path length
+ * @char: reads and tokenize character length
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-	char buffer[BUFFER SIZE];
-
-	char *command
-	char **arguments;
-	char *path = getenv("PATH");
-	char *path_dir[MAX_PATH_LENGTH / 2];
-	char *token = strtok(path, ":");
-	int i = 0;
+char *path = getenv("PATH");
+char *path_dir[MAX_PATH_LENGTH / 2];
+char *token = strtok(path, ":");
+int i = 0;
 while (token != NULL)
-	{
-		path_dir[i++] = token;
-		token = strtok(NULL, ":");
-	}
-	path_dir[e] = NULL;
-while
-	{
-	char user_input[MAX_INPUT_LENGTH];
-	printf("$ ");
-/* Read user input and length */
-	fgets(user_input, MAX_INPUT_LENGHT, stdin);
-/* Remove the newline character */	
-	user_input[strcspn(user_input, "\n")] = '\0';
-	}
+{
+path_dir[i++] = token;
+token = strtok(NULL, ":");
+}
+path_dir[e] = NULL;
+
+while (1)
+{
+char user_input[MAX_INPUT_LENGTH];
+printf("$ ");
+fgets(user_input, MAX_INPUT_LENGTH, stdin);
+user_input[strcspn(user_input, "\n")] = '\0';
+
 if (strcmp(user_input, "exit") == 0)
-	{
-	break;
-	}
-	char *command = strtok(user_input, " ");
-	if (command == NULL)
-	{
-		continue;
-	}
+break;
 
-	char cmd_path[MAX_LENGTH};
-	find_command (path_dir, command, cmd_path);
-	if (cmd_path[0] != '\0')
-	{
-		pid_t pid = fork();
-		if (pid == 0)
-		{
-			execvp(cmd_path, &command);
-			perror("Error command, execvp");
-			exit(EXIT_FAILURE);
-		}
+char *command = strtok(user_input, " ");
+if (command == NULL)
+{
+continue;
 
-		else if (pid < 0)
-		{
-			perror("fork");
-		}
-		else
-		{
-			wait(NULL);
-		}
-		else
-		{
-			printf("Command '%s' not found or not executable.\n", command);
-		}
-	}
-	return (0);
+char cmd_path[MAX_PATH_LENGTH];
+if (find_command(path_dir, command, cmd_path) == 0)
+{
+char **arguments = strtok_r(NULL, " ", &token);
+execute_command(cmd_path, arguments);
+}
+else
+{
+printf("Command '%s' not found or not executable.\n", command);
+}
+}
+return (0);
 }
